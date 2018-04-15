1bb478ddb8c7eb9ba64efe58fa9bdf7851ee2621
Bryan Adam Loh <bryanadamloh97@gmail.com>
2018-04-15 20:37:20 +0800

    Major Changes 2

--- js/animate.js
+++ js/animate.js
@@ -70,16 +70,104 @@ var whyAnimate = function() {
 	}
 };
 
+//Language
+var languageAnimate = function() {
+	if($('#language').length > 0) {
+
+		$('#language').waypoint( function ( direction ) {
+			if(direction === 'down' && !$(this.element).hasClass('animated')) {
+				setTimeout(function () {
+					$('#language .to-animate').each(function ( k ) {
+						var el = $(this);
+						
+						setTimeout(function () {
+							el.addClass('fadeInLeft animated');
+						}, k * 200, 'easeInOutExpo');
+					});
+				}, 200);
+
+				$(this.element).addClass('animated');
+			}
+		}, { offset: '80%'});
+	}
+};
+
+//Callout
+var calloutAnimate = function() {
+	if($('#callout').length > 0) {
+
+		$('#callout').waypoint( function ( direction ) {
+			if(direction === 'down' && !$(this.element).hasClass('animated')) {
+				setTimeout(function () {
+					$('#callout .to-animate').each(function( k ){
+						var el = $(this);
+
+						setTimeout( function (){
+							el.addClass('fadeIn animated');
+						}, k * 300, 'easeInOutExpo');
+					});
+				}, 300);
+
+				$(this.element).addClass('animated');
+			}
+		}, { offset: '80%'});
+	}
+};
+
+//Project
+var projectAnimate = function() {
+	if( $('#project').length > 0) {
+
+		$('#project').waypoint( function ( direction ) {
+			if(direction === 'down' && !$(this.element).hasClass('animated')) {
+				setTimeout(function() {
+					$('#project .to-animate').each(function( k ) {
+						var el = $(this);
+
+						setTimeout( function () {
+							el.addClass('fadeInRight animated');
+						}, k * 200, 'easeInOutExpo');
+					});
+				}, 200);
+
+				$(this.element).addClass('animated');
+			}
+		}, { offset: '80%'});
+	}
+};
+
+//Contact
+var contactAnimate = function() {
+	if( $('#contact').length > 0) {
+
+		$('#contact').waypoint( function ( direction ) {
+			if(direction === 'down' && !$(this.element).hasClass('animated')) {
+				setTimeout(function() {
+					$('#contact .to-animate').each(function( k ) {
+						var el= $(this);
+
+						setTimeout( function (){
+							el.addClass('fadeInDown animated');
+						}, k * 200, 'easeInOutExpo');
+					});
+				}, 200);
+
+				$(this.element).addClass('animated');
+			}
+		}, { offset: '80%'});
+	}
+};
+
 //Doucment to load
 $(function(){
 	headerAnimate();
 	aboutAnimate();
 	whyAnimate();
+	languageAnimate();
+	calloutAnimate();
+	projectAnimate();
+	contactAnimate();
 });
-
-
-
-
 
 
 }());
